ce65f3d5d445b188e4a683fa57f995bcc0f668a4
Hanya Adel <haniajed79@gmail.com>
2022-04-30 21:20:27 +0200

    Finished Graph input

--- source/Graph_Input.py
+++ source/Graph_Input.py
@@ -1,82 +1,139 @@
-from audioop import add
+from asyncio.windows_events import NULL
 from tkinter import *
 from Main_Page import directed, weighted
+from collections import defaultdict
 
-nodes = list()
+
+nodes = []
 currLetter = "A"
-adj_list = {}
 
-GraphInputPage = Tk()
-GraphInputPage.geometry('200x550')
-GraphInputPage.title('Graph Input')
+adj_list = defaultdict(list)
+
+
+class Node:
+    def __init__(self, name : str, heur : int) :
+        self.name = name
+        self.heur = heur
+
 
 def addNode():
-    global nodes
+
+    addNodesBtn['state'] = DISABLED
+    singleNodeFrame = Frame(nodesFrame, width = 70, height=120)
     global currLetter
-    nodes.append(currLetter)
-    currLetter = chr(ord(currLetter)+1) 
-    print(currLetter)
+    CurrNodeLabel = Label(singleNodeFrame, text = currLetter)
+    CurrNodeLabel.pack()
 
-def printGraph():
-  for node in adj_list:
-    print(node, " ---> ", [i for i in adj_list[node]])
+    heurLabel = Label(singleNodeFrame, text = "Enter Node Heuristic")
+    heurInput = Text(singleNodeFrame, height = 1, width = 4)  
+    heurLabel.pack()
+    heurInput.pack()  
 
-def AddEdge(node1, node2, weight):
-    print ("clickedd")
-    temp = []
-    if node1 in nodes and node2 in nodes:
-        if node1 not in adj_list:
-            temp.append([node2,weight])
-            adj_list[node1] = temp
-   
-        elif node1 in adj_list:
-            temp.extend(adj_list[node1])
-            temp.append([node2,weight])
-            adj_list[node1] = temp
-    
+    def submitNode():
+        addNodesBtn['state'] = NORMAL
+        submitNodeBtn['state'] = DISABLED
+        heurInput.config(state = DISABLED)
+
+        global currLetter
+        nodes.append(Node(currLetter, heurInput.get("1.0",END)))
+        currLetter = chr(ord(currLetter)+1) 
+        
+        
+    submitNodeBtn = Button(singleNodeFrame, text = "Submit Node", command = submitNode)
+    submitNodeBtn.pack()
+
+    singleNodeFrame.pack(side = LEFT)
+
+
+def LockNodes():
+    addEdgesBtn['state'] = NORMAL
+    addNodesBtn["state"] = DISABLED
+    LockNodesBtn['state'] = DISABLED
+
+
+
+def addEdge():
     printGraph()
+    addEdgesBtn['state'] = DISABLED
 
+    singleEdgeFrame = Frame(GraphInputPage, width=100, height=170)  
 
-def ShowAddingEdgesMenu():
-    src = StringVar() 
+    nodeValues = []
+    for node in nodes:
+        nodeValues.append(node.name)
+    
+
+    src = StringVar()
     src.set( "Select Source" )
-    srcDrop = OptionMenu( GraphInputPage , src , *nodes )
+    srcDrop = OptionMenu( singleEdgeFrame , src , *nodeValues )
     srcDrop.pack() 
 
     dest = StringVar() 
     dest.set( "Select Destination" )
-    destDrop = OptionMenu( GraphInputPage , dest , *nodes )
+    destDrop = OptionMenu( singleEdgeFrame , dest , *nodeValues )
     destDrop.pack()   
 
-    weight = 0
+    label = Label(singleEdgeFrame, text = "enter weight")
+    weightInput = Text(singleEdgeFrame, height = 1, width = 4)  
     if weighted == TRUE:
-        label = Label(GraphInputPage, text = "enter weight")
-        T = Text(GraphInputPage, height = 5, width = 10)  
         label.pack()
-        T.pack()
-        weight = T.get("1.0",END)
-    
-    addBtn = Button(GraphInputPage, text = "Add", command = AddEdge(src, dest, weight))
-    addBtn.pack()
+        weightInput.pack()
+        
+    def submitEdge():
+        addEdgesBtn["state"] = NORMAL
+        submitEdgeBtn['state'] = DISABLED
+        weightInput.config(state = DISABLED)
+
+        srcNode, destNode = NULL, NULL
+
+        for node in nodes:
+            if node.name == src.get():
+                srcNode = node
+            if node.name == dest.get():
+                destNode = node
+        
+        weight = weightInput.get("1.0",END)
+
+        temp = [destNode, weight]
+        adj_list[srcNode].append (temp)
+
+        if directed == FALSE: 
+            temp = [srcNode, weight]
+            adj_list[destNode].append (temp)
+        
+
+    submitEdgeBtn = Button(singleEdgeFrame, text = "Submit Edge", command = submitEdge)
+    submitEdgeBtn.pack()
+
+    singleEdgeFrame.pack(side = LEFT)
 
 
-def LockNodes():
-    print("Locked Nodes")  
-    addEdgesBtn= Button(GraphInputPage, text="Add Edge", command= ShowAddingEdgesMenu)
-    addEdgesBtn.pack()
-    
+def printGraph():
+    for node in adj_list:
+        for edges in adj_list[node]:
+            print(node.name, "-->", edges[0].name, edges[1])
 
 
 
-label = Label(GraphInputPage, text = " ")
-label.pack()
-button1= Button(GraphInputPage, text="Add Node", command= addNode)
-button1.pack(ipadx=10, ipady=10)
 
-label2 = Label(GraphInputPage, text = " ")
-label2.pack()
-LockNodesBtn= Button(GraphInputPage, text="Lock Nodes", command= LockNodes)
+GraphInputPage = Tk()
+GraphInputPage.geometry('700x550')
+GraphInputPage.title('Graph Input')
+
+nodesFrame = Frame(GraphInputPage, width=550, height=200)
+nodesFrame.pack()
+nodesFrame.pack_propagate(0)
+
+addNodesLabel = Label (nodesFrame, text = "Adding Nodes")
+addNodesLabel.pack()
+
+addNodesBtn= Button(nodesFrame, text="Click to add new node", command= addNode)
+addNodesBtn.pack(ipadx=10, ipady=10)
+
+LockNodesBtn= Button(nodesFrame, text="Click to Lock all Nodes", command= LockNodes)
 LockNodesBtn.pack(ipadx=10, ipady=10)
 
+addEdgesBtn= Button(GraphInputPage, text="Click to add new edge", state = DISABLED, command= addEdge)
+addEdgesBtn.pack()
 
 GraphInputPage.mainloop()
