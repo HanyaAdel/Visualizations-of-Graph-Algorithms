0e7c34bcaf419143dcef6fcf12221b1c35489654
Sara Hussein <85261712+SaraHussein28@users.noreply.github.com>
2022-05-11 22:05:12 +0200

    Updated bfs and greedy-search

--- source/Graph_algorithms.py
+++ source/Graph_algorithms.py
@@ -1,80 +1,158 @@
+import sys
+from Graph import Node, nodes, adj_list
 from queue import PriorityQueue
 
 
 class Algorithms:
-    graph = []
     found = False
     visited_return = []     # visited list to be returned
-    visited = []            # visited list to prevent infinite loops
+    visited_return_ID = []  # visited list to be returned for iterative deepening algorithm
+    visited_flag = {}       # to avoid duplicates in visited_return (also consider using ordered sets to eliminate this)
+    visited = {}            # visited list to prevent infinite loops
+    visited_path = []       # list to be used for illustrating the algorithm path on the graph
     path = []
     queue = []              #queue for bfs function
-    solutionPath = []
-    parent = []
-    heuristics = []
+    parent = {}             #to generate solution path 
+    totalCost = 0           #returns total cost from source to destination
 
-    def __init__(self, num_nodes, graph):
-        self.graph = graph
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.clear_visited_return()
+        self.reset_iterative_deepening()
+        self.visited_path.clear()
+        self.visited_return_ID.clear()
+
+    def clear_visited_return(self):
+        self.visited_return.clear()
+        self.reset_visited_flag()
+
+    def reset_visited_flag(self):
+        for node in nodes:
+            self.visited_flag[node] = False
+
+    def reset_visited(self):
+        for node in nodes:
+            self.visited[node] = False
+
+    def reset_iterative_deepening(self):
+        self.path.clear()
         self.found = False
-        self.visited_return = [False for i in range(num_nodes)]
-        self.visited = [False for i in range(num_nodes)]
-        self.parent = [0 for i in range(num_nodes)]
-        self.path = []
-        self.solutionPath = []
-        self.queue = []
-        self.heuristics = []
+        self.reset_visited()
+        self.clear_visited_return()
 
     def get_visited(self):
         return self.visited_return
 
-    def get_solution_path(self):
-        return self.solutionPath
+    def get_visited_ID(self):                   # get visited lists for Iterative Deepening
+        return self.visited_return_ID
 
-    def generate_solution_path(self, source, goal):
-        node = goal
-        self.solutionPath.append(node)
-        while node != source:
-            node = self.parent[node]
-            self.solutionPath.append(node)
+    def get_path(self):
+        return self.path
 
-        self.solutionPath.reverse()
+    def get_visited_path(self):
+        return self.visited_path
+
+    def get_total_cost(self):
+        return self.totalCost
+
+
+    def depth_limited(self, source: Node, goal: Node, max_depth, depth=0):
+        self.path.append(source.name)
+        self.visited_path.append(source.name)
+        self.visited[source] = True
+
+        if not self.visited_flag[source]:
+            self.visited_return.append(source.name)
+            self.visited_flag[source] = True
+
+        if source == goal:
+            self.found = True
+            return
+        if depth != max_depth:
+            for child in adj_list[source]:
+                child_node = child[0]
+                if not self.visited[child_node]:
+                    # self.visited[child_node] = True
+                    self.depth_limited(child_node, goal, max_depth, depth + 1)
+                    if self.found:
+                        return
+                    self.visited[child_node] = False
+
+        self.path.pop()
+
+    def dfs(self, source, goal):
+        return self.depth_limited(source, goal, sys.maxsize)
+
+    def iterative_deepening(self, source, goal, max_depth):
+        for depth in range(max_depth):
+            self.reset_iterative_deepening()
+            self.depth_limited(source, goal, depth)
+            visited = self.visited_return.copy()
+            self.visited_return_ID.append(visited)
+            if self.found:
+                return
+
+
+
+    def generate_solution_path_and_calculate_total_cost(self, source:Node, goal:Node):
+        node = [goal, 0]
+        self.path.append(node[0].name)
+       
+        while node[0] != source:
+            node = self.parent[node[0]]
+            self.path.append(node[0].name)
+            self.totalCost+= node[1]
+        self.path.reverse()
+       
         return
 
-    def bfs(self, source, goal):
-        self.visited_return[source] = True
+    def bfs(self, source: Node, goal:Node):
         self.queue.append(source)
-
+        self.visited_flag[source] == True
+        self.visited_return.append(source.name)
+        
         while self.queue:          # Creating loop to visit each node
             frontNode = self.queue.pop(0) 
+            self.visited_path.append(frontNode.name)
             
+
             if frontNode == goal:
-                self.generate_solution_path(source, goal)
+                self.generate_solution_path_and_calculate_total_cost(source, goal)
                 self.found = True
                 return
             
-            for child in self.graph[frontNode]:
-                if not self.visited_return[child]:
-                     self.visited_return[child] = True
-                     self.parent[child] = frontNode
-                     self.queue.append(child)
+            for child in adj_list[frontNode]:
+                if not self.visited_flag[child[0]]:
+                    self.visited_flag[child[0]] = True
+                    self.visited_return.append(child[0].name)
+                    self.parent[child[0]] = [frontNode, child[1]]
+                    self.queue.append(child[0])
 
 
-    def greedy_best_first_search(self, source, goal, heuristics):
-        self.heuristics = heuristics
-        self.visited_return[source] = True
+    def greedy_best_first_search(self, source: Node, goal: Node):
+
+        self.visited_flag[source] == True
+        self.visited_return.append(source.name)
 
         pq = PriorityQueue()
-        pq.put((self.heuristics[source], source))
+        pq.put((source.heuristic, source))
         
         while pq.empty() == False:
             topNode = pq.get()[1]
+            self.visited_path.append(topNode.name)
 
             if topNode == goal:
-                self.generate_solution_path(source, goal)
+                self.generate_solution_path_and_calculate_total_cost(source, goal)
+                self.found = True
                 return
     
-            for childNode in self.graph[topNode]:
-                if self.visited_return[childNode] == False:
-                    self.visited_return[childNode] = True
-                    self.parent[childNode] = topNode
-                    pq.put((self.heuristics[childNode], childNode))
-        
+            for childNode in adj_list[topNode]:
+                if self.visited_flag[childNode[0]] == False:
+                    self.visited_flag[childNode[0]] = True
+                    self.visited_return.append(childNode[0].name)
+                    self.parent[childNode[0]] = [topNode, childNode[1]]
+                    pq.put((childNode[0].heuristic, childNode[0]))
+ 
+
